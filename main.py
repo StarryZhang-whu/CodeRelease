03fe7d42e7f7ef22be474416e87555167fb5dd3a
StarryZhang-whu <93083801+StarryZhang-whu@users.noreply.github.com>
2023-05-17 17:15:14 +0800

    add test script

--- main.py
+++ main.py
@@ -1,16 +1,59 @@
 # This is a sample Python script.
-
+import json
+import requests
+from bs4 import BeautifulSoup
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+token = "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"
+
+data = {
+    'repo': 'vuejs/vue',
+    'stars': 179271,
+    'language': 'JavaScript',
+    'note_url': 'https://github.com/vuejs/vue/releases/tag/v2.6.8',
+    'commits_url': 'https://github.com/vuejs/vue/compare/v2.6.7...v2.6.8'
+}
+
+headers = {
+    "Authorization": f"token {token}",
+    "Accept": "application/vnd.github.v3+json"
+}
+
+# Github API base URL
+base_url = 'https://api.github.com/repos/'
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Get commit diffs
+commits_url = data['commits_url'].replace('https://github.com/', base_url) + '.diff'
+headers["Accept"] = "application/vnd.github.diff"
+response = requests.get(commits_url, headers=headers)
 
+# Check if request was successful
+if response.status_code == 200:
+    diff = response.text
+else:
+    diff = 'Could not fetch data'
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Get release notes
+note_url = data['note_url']
+response = requests.get(note_url)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Check if request was successful
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, 'html.parser')
+    notes = soup.find('div', {'class': 'markdown-body'}).get_text()
+else:
+    notes = 'Could not fetch data'
+
+# Prepare data for JSON
+data_point = {
+    'repo': data['repo'],
+    'stars': data['stars'],
+    'language': data['language'],
+    'release_notes': notes,
+    'commits': diff.split('\n\n')
+}
+
+# Save data to JSON file
+with open('data.json', 'w') as file:
+    json.dump(data_point, file, indent=4)
