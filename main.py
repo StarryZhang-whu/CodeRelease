c2dd63b4267f196f39989716eb403fdf5312560f
StarryZhang-whu <93083801+StarryZhang-whu@users.noreply.github.com>
2023-05-19 16:28:16 +0800

    demo

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 
 # configure GitHub api
-token = os.getenv('GITHUB_TOKEN')
+token = "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"
 headers = {
     "Authorization": f"token {token}",
     "Accept": "application/vnd.github.v3+json"
@@ -29,22 +29,35 @@ def get_diff(repo, start_sha, end_sha):
 
 
 for data in versions:
+    headers["Accept"] = "application/vnd.github.v3+json"
     # 获取版本的提交信息
-    commit_url = data['commits_url'].replace('compare', 'commits')
-    commits_data = requests.get(commit_url, headers=headers).json()
-
-    # 获取版本之间的所有commit
+    commit_url = data['commits_url'].replace('https://github.com/', 'https://api.github.com/repos/')
+    print("Now request:", commit_url)
+    response = requests.get(commit_url, headers=headers)
+    # commits_data = requests.get(commit_url, headers=headers).json()
     commits = []
-    if commits_data and 'commits' in commits_data:
-        for i in range(1, len(commits_data['commits'])):
-            commit_info = {'sha': commits_data['commits'][i]['sha'], 'commit_message': commits_data['commits'][i]['commit']['message']}
-            diff = get_diff(data['repo'], commits_data['commits'][i-1]['sha'], commits_data['commits'][i]['sha'])
+    if response.status_code == 200:
+        print("Request commits success")
+        # 获取版本之间的所有commit
+        try:
+            commits_data = response.json()
+        except json.decoder.JSONDecodeError as e:
+            print("Failed to decode JSON: ", e)
+            print("Response: ", response.text)
+            continue
+        print("Json parser success")
+        raw_commits = commits_data['commits']
+        for i in range(1, len(raw_commits)):
+            commit_info = {'sha': raw_commits[i]['sha'], 'commit_message': raw_commits[i]['commit']['message']}
+            diff = get_diff(data['repo'], raw_commits[i - 1]['sha'], raw_commits[i]['sha'])
             commit_info['diff'] = diff
             commits.append(commit_info)
-
+    else:
+        print("Error: ", response.status_code)
     # 获取版本的发布说明
-    response = requests.get(data['note_url'])
+    response = requests.get(data['note_url'], headers=headers)
     if response.status_code == 200:
+        print("Request release notes success")
         soup = BeautifulSoup(response.text, 'html.parser')
         notes = soup.find('div', {'class': 'markdown-body'}).get_text()
     else:
